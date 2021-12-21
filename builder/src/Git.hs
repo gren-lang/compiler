c80bfb8e6af94235ab5c829965b2e273b9f26b84
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2021-12-21 21:02:01 +0100

    Added clone function to Git module. Should probably catch and format errors.

--- builder/src/Git.hs
+++ builder/src/Git.hs
@@ -1,10 +1,13 @@
 module Git
     ( checkInstalledGit
+    , clone
     )
     where
 
 
 import System.Directory (findExecutable)
+import qualified System.IO as IO
+import qualified System.Process as Process
 
 
 checkInstalledGit :: IO Bool
@@ -14,3 +17,13 @@ checkInstalledGit = do
      Just _ -> True
      Nothing -> False
 
+
+clone :: String -> String -> IO ()
+clone targetFolder gitUrl = do
+    nullHandle <- IO.openFile "/dev/null" IO.WriteMode
+    procResult <- 
+        Process.createProcess (Process.proc "git" [ "clone", gitUrl, targetFolder ]) 
+            { Process.std_out = Process.UseHandle nullHandle
+            , Process.std_err = Process.UseHandle nullHandle
+            }
+    return ()
