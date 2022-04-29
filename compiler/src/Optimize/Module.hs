881709ca6c613bb222d290445a3a6f9fd5964593
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-04-29 15:01:51 +0200

    Remove unused variable.

--- compiler/src/Optimize/Module.hs
+++ compiler/src/Optimize/Module.hs
@@ -35,7 +35,7 @@ type Annotations =
   Map.Map Name.Name Can.Annotation
 
 optimize :: Annotations -> Can.Module -> Result i [W.Warning] Opt.LocalGraph
-optimize annotations (Can.Module home _ _ decls unions aliases _ effects) =
+optimize annotations (Can.Module home _ _ decls unions _ _ effects) =
   addDecls home annotations decls $
     addEffects home effects $
       addUnions home unions $
