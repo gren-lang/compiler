7fd2dae209ddac0caa3bb444044a1fe4f3885b5e
jjant <jantonielli@itba.edu.ar>
2022-05-25 19:21:21 +0100

    Refactor that breaks parsing

--- compiler/src/Parse/Expression.hs
+++ compiler/src/Parse/Expression.hs
@@ -171,17 +171,19 @@ record start =
             word1 0x7D {-}-} E.RecordOpen
             addEnd start (Src.Record []),
           do
-            (A.At reg name) <- addLocation (Var.lower E.RecordField)
-            Space.chompAndCheckIndent E.RecordSpace E.RecordIndentEquals
             oneOf
               E.RecordEquals
               [ do
+                  (A.At reg name) <- addLocation (Var.lower E.RecordField)
+                  Space.chompAndCheckIndent E.RecordSpace E.RecordIndentEquals
                   word1 0x7C {- vertical bar -} E.RecordEquals
                   Space.chompAndCheckIndent E.RecordSpace E.RecordIndentField
                   firstField <- chompField
                   fields <- chompFields [firstField]
                   addEnd start (Src.Update (A.At reg (Src.Var Src.LowVar name)) fields),
                 do
+                  (A.At reg name) <- addLocation (Var.lower E.RecordField)
+                  Space.chompAndCheckIndent E.RecordSpace E.RecordIndentEquals
                   word1 0x3D {-=-} E.RecordEquals
                   Space.chompAndCheckIndent E.RecordSpace E.RecordIndentExpr
                   (value, end) <- specialize E.RecordExpr expression
