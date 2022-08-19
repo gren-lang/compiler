02c5fe8c9f8d434857d31f49ac0f8b0c11b75194
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-08-19 09:59:22 +0200

    Init now finds the latest compatible version of the initial packages.

--- terminal/src/Init.hs
+++ terminal/src/Init.hs
@@ -6,10 +6,12 @@ module Init
   )
 where
 
+import Data.Either qualified as Either
 import Data.Map qualified as Map
 import Data.NonEmptyList qualified as NE
 import Deps.Package qualified as DPkg
 import Deps.Solver qualified as Solver
+import Directories qualified as Dirs
 import Gren.Constraint qualified as Con
 import Gren.Licenses qualified as Licenses
 import Gren.Outline qualified as Outline
@@ -73,16 +75,17 @@ init flags =
           if _isPackage flags
             then pkgDefaultDeps
             else appDefaultDeps
-    deps <- mapM (DPkg.getLatestCompatibleVersion cache) initialDeps
     (Solver.Env cache) <- Solver.initEnv
+    depsM <- mapM (compatibleDependencies cache) initialDeps
+    let deps = Map.fromList $ Either.rights depsM
     result <- Solver.verify cache deps
     case result of
       Solver.Err exit ->
         return (Left (Exit.InitSolverProblem exit))
       Solver.NoSolution ->
-        return (Left (Exit.InitNoSolution (Map.keys deps)))
+        return (Left (Exit.InitNoSolution initialDeps))
       Solver.NoOfflineSolution ->
-        return (Left (Exit.InitNoOfflineSolution (Map.keys deps)))
+        return (Left (Exit.InitNoOfflineSolution initialDeps))
       Solver.Ok details ->
         let outline =
               if _isPackage flags
@@ -93,6 +96,15 @@ init flags =
               Outline.write "." outline
               putStrLn "Okay, I created it."
               return (Right ())
+
+compatibleDependencies :: Dirs.PackageCache -> Pkg.Name -> IO (Either () (Pkg.Name, Con.Constraint))
+compatibleDependencies cache pkgName = do
+  possibleVersion <- DPkg.getLatestCompatibleVersion cache pkgName
+  case possibleVersion of
+    Left _ ->
+      return $ Left ()
+    Right vsn ->
+      return $ Right (pkgName, Con.untilNextMajor vsn)
 
 pkgOutline :: Map.Map Pkg.Name Con.Constraint -> Outline.Outline
 pkgOutline deps =
@@ -110,8 +122,9 @@ pkgOutline deps =
 appOutlineFromSolverDetails :: (Map.Map Pkg.Name Solver.Details) -> Outline.Outline
 appOutlineFromSolverDetails details =
   let solution = Map.map (\(Solver.Details vsn _) -> vsn) details
-      directs = Map.intersection solution appDefaultDeps
-      indirects = Map.difference solution appDefaultDeps
+      defaultDeps = Map.fromList $ map (\dep -> (dep, Con.anything)) appDefaultDeps
+      directs = Map.intersection solution defaultDeps
+      indirects = Map.difference solution defaultDeps
    in Outline.App $
         Outline.AppOutline
           V.compiler
