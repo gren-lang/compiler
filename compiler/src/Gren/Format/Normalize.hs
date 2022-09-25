0de3e9823afde2633d262f52c01b2a0eb8366e64
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-09-25 12:36:40 +0200

    Formatting.

--- compiler/src/Gren/Format/Normalize.hs
+++ compiler/src/Gren/Format/Normalize.hs
@@ -7,15 +7,15 @@
 -- as a responsibility for the code that is rendering the AST into text.
 module Gren.Format.Normalize (normalize) where
 
-import Gren.Package qualified as Pkg
 import AST.Source qualified as Src
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (mapMaybe)
 import Data.Name (Name)
 import Gren.Compiler.Imports qualified as Imports
-import Reporting.Annotation qualified as A
+import Gren.Package qualified as Pkg
 import Parse.Module qualified as Parse
+import Reporting.Annotation qualified as A
 
 normalize :: Parse.ProjectType -> Src.Module -> Src.Module
 normalize projectType module_ =
@@ -35,11 +35,12 @@ removeDefaultImports projectType import_@(Src.Import name alias exposing) =
 defaultImports :: Parse.ProjectType -> Map Name Src.Import
 defaultImports projectType =
   case projectType of
-    Parse.Package pkg | pkg == Pkg.core ->
-        Map.empty
+    Parse.Package pkg
+      | pkg == Pkg.core ->
+          Map.empty
     _ ->
-        Map.fromList $
-            fmap (\import_ -> (Src.getImportName import_, import_)) Imports.defaults
+      Map.fromList $
+        fmap (\import_ -> (Src.getImportName import_, import_)) Imports.defaults
 
 exposingEq :: Src.Exposing -> Src.Exposing -> Bool
 exposingEq Src.Open Src.Open = True
