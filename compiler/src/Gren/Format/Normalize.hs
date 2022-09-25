0e03a4b42e1c9f709fdbcdcac673612bb98fdcb8
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-09-25 12:36:03 +0200

    Only remove default imports when package isnt core.

--- compiler/src/Gren/Format/Normalize.hs
+++ compiler/src/Gren/Format/Normalize.hs
@@ -7,33 +7,39 @@
 -- as a responsibility for the code that is rendering the AST into text.
 module Gren.Format.Normalize (normalize) where
 
+import Gren.Package qualified as Pkg
 import AST.Source qualified as Src
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (mapMaybe)
 import Data.Name (Name)
-import Gren.Compiler.Imports qualified
+import Gren.Compiler.Imports qualified as Imports
 import Reporting.Annotation qualified as A
 import Parse.Module qualified as Parse
 
 normalize :: Parse.ProjectType -> Src.Module -> Src.Module
 normalize projectType module_ =
   module_
-    { Src._imports = mapMaybe removeDefaultImports $ Src._imports module_
+    { Src._imports = mapMaybe (removeDefaultImports projectType) $ Src._imports module_
     }
 
-removeDefaultImports :: Src.Import -> Maybe Src.Import
-removeDefaultImports import_@(Src.Import name alias exposing) =
-  case Map.lookup (A.toValue name) defaultImports of
+removeDefaultImports :: Parse.ProjectType -> Src.Import -> Maybe Src.Import
+removeDefaultImports projectType import_@(Src.Import name alias exposing) =
+  case Map.lookup (A.toValue name) (defaultImports projectType) of
     Just (Src.Import _ defAlias defExposing) ->
       if alias == defAlias && exposingEq exposing defExposing
         then Nothing
         else Just import_
     Nothing -> Just import_
 
-defaultImports :: Map Name Src.Import
-defaultImports =
-  Map.fromList $ fmap (\import_ -> (Src.getImportName import_, import_)) Gren.Compiler.Imports.defaults
+defaultImports :: Parse.ProjectType -> Map Name Src.Import
+defaultImports projectType =
+  case projectType of
+    Parse.Package pkg | pkg == Pkg.core ->
+        Map.empty
+    _ ->
+        Map.fromList $
+            fmap (\import_ -> (Src.getImportName import_, import_)) Imports.defaults
 
 exposingEq :: Src.Exposing -> Src.Exposing -> Bool
 exposingEq Src.Open Src.Open = True
