58b1b6098af08887ca018e87f4953549b47d101c
Robin Heggelund Hansen <git@heghan.org>
2024-09-23 23:03:36 +0200

    Add make-static command for creating SEAs.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
 const compiler = require("./compiler.js");
+const compilerInstance = compiler.Gren.Main.init({});
 
-compiler.Gren.Main.init({});
+compilerInstance.ports.completeStaticBuild.subscribe(function(output) {
+  console.log("Make static build out of " + output);
+});
