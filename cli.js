ab8445357e2cc4e5d3f8a68e8e5a2d1758a2c5b3
Robin Heggelund Hansen <git@heghan.org>
2024-12-13 20:55:19 +0100

    Fix issues with make-static command.

--- cli.js
+++ cli.js
@@ -28,54 +28,64 @@ compilerInstance.ports.completeStaticBuild.subscribe(async function (output) {
   // the Gren application, with a hint that tells the V8 engine what the
   // main function is
 
-  const compiledSrc = await fs.readFile(jsBuildPath, "utf-8");
+  try {
+    const compiledSrc = await fs.readFile(jsBuildPath, "utf-8");
 
-  const initRegex = /this\.Gren\..+\(\{\}\);/g;
-  const initCall = compiledSrc.match(initRegex)[0];
-  const snapshotCompatibleSrc = compiledSrc.replace(
-    initCall,
-    `
-const v8 = require('node:v8');
-v8.startupSnapshot.setDeserializeMainFunction(function() {
-  ${initCall}
-});
-`,
-  );
-
-  await fs.writeFile(jsBuildPath, snapshotCompatibleSrc);
-
-  // We then need to generate the snapshot
-
-  const nodePath = process.execPath;
-  await fs.writeFile(seaConfigPath, JSON.stringify(seaConfig));
-  cp.execFileSync(nodePath, ["--experimental-sea-config", seaConfigPath]);
-
-  // Then copy the node executable and inject the snapshot into it
-  await fs.copyFile(nodePath, binPath);
-
-  if (isMac) {
-    // required on mac, optional on windows, not required on linux
-    cp.execFileSync("codesign", ["--remove-signature", binPath]);
-  }
-
-  const blobContent = await fs.readFile(blobPath);
-
-  await postject.inject(binPath, "NODE_SEA_BLOB", blobContent, {
-    sentinelFuse: "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2",
-    machoSegmentName: isMac ? "NODE_SEA" : undefined,
+    const initRegex = /this\.Gren\..+\(\{\}\);/g;
+    const initCall = compiledSrc.match(initRegex)[0];
+    const snapshotCompatibleSrc = compiledSrc.replace(
+      initCall,
+      `
+  const v8 = require('node:v8');
+  v8.startupSnapshot.setDeserializeMainFunction(function() {
+    ${initCall}
   });
+  `,
+    );
 
-  if (isMac) {
-    // required on mac
-    cp.execFileSync("codesign", ["--sign", "-", binPath]);
+    await fs.writeFile(jsBuildPath, snapshotCompatibleSrc);
+
+    // We then need to generate the snapshot
+
+    const nodePath = process.execPath;
+    await fs.writeFile(seaConfigPath, JSON.stringify(seaConfig));
+    cp.execFileSync(nodePath, ["--experimental-sea-config", seaConfigPath]);
+
+    // Then copy the node executable and inject the snapshot into it
+    await fs.copyFile(nodePath, binPath);
+    await fs.chmod(binPath, "755");
+
+    if (isMac) {
+      // required on mac, optional on windows, not required on linux
+      cp.execFileSync("codesign", ["--remove-signature", binPath]);
+    }
+
+    const blobContent = await fs.readFile(blobPath);
+
+    await postject.inject(binPath, "NODE_SEA_BLOB", blobContent, {
+      sentinelFuse: "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2",
+      machoSegmentName: isMac ? "NODE_SEA" : undefined,
+    });
+
+    if (isMac) {
+      // required on mac
+      cp.execFileSync("codesign", ["--sign", "-", binPath]);
+    }
+
+    const outputPath = isWin ? jsBuildPath + ".exe" : jsBuildPath;
+    await fs.rename(binPath, outputPath);
+
+    if (isWin) {
+      await fs.rm(jsBuildPath);
+    }
+
+    console.log("Done!");
+  } catch (e) {
+    console.error("Failed to create static executable", e);
+    await fs.rm(jsBuildPath);
+  } finally {
+    // cleanup
+    await fs.rm(blobPath);
+    await fs.rm(seaConfigPath);
   }
-
-  // cleanup
-
-  await fs.rm(jsBuildPath);
-  await fs.rm(blobPath);
-  await fs.rm(seaConfigPath);
-
-  const outputPath = isWin ? jsBuildPath + ".exe" : jsBuildPath;
-  await fs.rename(binPath, outputPath);
 });
