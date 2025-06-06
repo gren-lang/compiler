d963ecdec91fc270a9d09312d7d0618bf271e0db
Robin Heggelund Hansen <git@heghan.org>
2025-06-06 14:39:15 +0200

    Remove node version check, now that 18 is eol.

--- compiler/src/Generate/Node.hs
+++ compiler/src/Generate/Node.hs
@@ -12,16 +12,12 @@ import Data.Name qualified as Name
 import Text.RawString.QQ (r)
 
 leadingLines :: Int
-leadingLines = 7
+leadingLines = 3
 
 sandwich :: Name.Name -> B.Builder -> B.Builder
 sandwich moduleName javascript =
   let name = Name.toBuilder moduleName
    in [r|#!/usr/bin/env node
-
-if (parseInt(process.versions.node.split('.')[0]) < 20) {
-  throw new Error("This program requires Node v20 or later to run")
-}
 
 try {
 |]
