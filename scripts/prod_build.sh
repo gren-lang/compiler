7d1b1a7f9fabe4af87f8df85302504ccb4c6ecad
Robin Heggelund Hansen <git@heghan.org>
2025-07-18 10:49:06 +0200

    Move prod build of compiler to bin/compiler, to avoid commiting new compilers all the time.

--- scripts/prod_build.sh
+++ scripts/prod_build.sh
@@ -3,7 +3,7 @@
 set -e
 
 npx --package=gren-lang@0.5 gren make src/Main.gren --optimize --output=app
-terser app -c -m -o app
+terser app -c -m -o bin/compiler
 
 # Check to see that we haven't changed the state of the repo
 # If this fails, a new commit should be made prior to publishing
