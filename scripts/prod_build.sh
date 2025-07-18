5901dccfb90aab876c0b85783652742c7c827510
Robin Heggelund Hansen <git@heghan.org>
2025-07-18 10:33:05 +0200

    Commit built compiler to repo.

---
+++ scripts/prod_build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+npx --package=gren-lang@0.5 gren make src/Main.gren --optimize --output=app
+terser app -c -m -o app
+
+# Check to see that we haven't changed the state of the repo
+# If this fails, a new commit should be made prior to publishing
+test -z "$(git status --porcelain)" || (echo "production build of compiler is not commited!" && exit 1)
